47b0e219672207f626a8bf23ead68630e1a8faba
vengadora <vengadoravg@gmail.com>
2015-01-24 10:19:36 -0400

    movement of the main character

--- SnowLevel.js
+++ SnowLevel.js
@@ -14,6 +14,10 @@ SnowLevel.prototype.create = function () {
     game.stage.backgroundColor = '#eee';
     this.world.create();
     this.you.create();
+
+    this.cursor = game.input.keyboard.createCursorKeys();
+    this.cursor.zoom = game.input.keyboard.addKey(Phaser.Keyboard.Z);
+    this.you.cursor = this.cursor;
 };
 
 SnowLevel.prototype.update = function () {
--- Survivor.js
+++ Survivor.js
@@ -1,5 +1,8 @@
-var Survivor = function (world) {
+var Survivor = function (world, cursor) {
+    this.speed = 200;
+
     this.world = world;
+    this.cursor = cursor;
 };
 
 Survivor.prototype.preload = function () {
@@ -7,11 +10,23 @@ Survivor.prototype.preload = function () {
 };
 
 Survivor.prototype.create = function () {
-    this.sprite = game.add.sprite(100, 400, 'sprite');
+    this.sprite = game.add.sprite(150, 400, 'sprite');
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
     this.sprite.body.acceleration.y = 481;
+    this.sprite.anchor.set(1/2, 1/2);
 };
 
 Survivor.prototype.update = function () {
     game.physics.arcade.collide(this.world.floor, this.sprite);
+    this.sprite.body.velocity.x = 0;
+
+    if (this.cursor.right.isDown) {
+        this.sprite.body.velocity.x = this.speed;
+        this.sprite.scale.set(1,1);
+    }
+
+    if (this.cursor.left.isDown) {
+        this.sprite.body.velocity.x = -this.speed;
+        this.sprite.scale.set(-1,1);
+    }
 };
