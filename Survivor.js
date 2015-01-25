64d035f22ed3f07b16f54028ce7da0c624ece50f
Ronald Valverde <ronald.valverde@addvisorygroup.com>
2015-01-24 21:06:47 -0400

    adding footstep sfx

--- Survivor.js
+++ Survivor.js
@@ -7,13 +7,16 @@ var Survivor = function (world, cursor) {
 
 Survivor.prototype.preload = function () {
     game.load.spritesheet('sprite', 'img/sprite/sprite.png',42, 160);
+    game.load.audio('stepsfx', ['sfx/snow_step.wav']);
 };
 
+var stepsfx;
 Survivor.prototype.create = function () {
     this.sprite = game.add.sprite(150, 400, 'sprite');
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
     this.sprite.body.acceleration.y = 481;
     this.sprite.anchor.set(1/2, 1/2);
+    stepsfx = game.add.audio('stepsfx');
 };
 
 Survivor.prototype.update = function () {
@@ -23,11 +26,19 @@ Survivor.prototype.update = function () {
     if (this.cursor.right.isDown) {
         this.sprite.body.velocity.x = this.speed;
         this.sprite.scale.set(1,1);
+        playStepSFX();
     }
 
     if (this.cursor.left.isDown) {
         this.sprite.body.velocity.x = -this.speed;
         this.sprite.scale.set(-1,1);
+        playStepSFX();
     }
 
 };
+
+playStepSFX = function () {
+    if (!stepsfx.isPlaying){
+        stepsfx.play();
+    }
+}
